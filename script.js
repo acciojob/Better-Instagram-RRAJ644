deceaeb760ba44954e3a2391c1cb07516cb566d8
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-08 21:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-let dragindex = 0
-let dropindex = 0
-let clone = ''
 
-const images = document.querySelectorAll('.image')
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+
+const images = document.querySelectorAll(".image");
 
 function drag(e) {
-  e.dataTransfer.setData('text', e.target.id)
+  e.dataTransfer.setData("text", e.target.id);
 }
 
 function allowDrop(e) {
-  e.preventDefault()
+  e.preventDefault();
 }
 
 function drop(e) {
-  clone = e.target.cloneNode(true)
-  let data = e.dataTransfer.getData('text')
-  let nodelist = document.getElementById('parent').childNodes
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
   for (let i = 0; i < nodelist.length; i++) {
     if (nodelist[i].id == data) {
-      dragindex = i
+      dragindex = i;
     }
   }
 
-  dragdrop(clone)
+  dragdrop(clone);
 
   document
-    .getElementById('parent')
-    .replaceChild(document.getElementById(data), e.target)
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
 
   document
-    .getElementById('parent')
+    .getElementById("parent")
     .insertBefore(
       clone,
-      document.getElementById('parent').childNodes[dragindex]
-    )
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
 
 const dragdrop = (image) => {
-  image.ondragstart = drag
-  image.ondragover = allowDrop
-  image.ondrop = drop
-}
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
 
-images.forEach(dragdrop)
+images.forEach(dragdrop);
