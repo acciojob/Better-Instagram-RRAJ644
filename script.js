f9976b7ff6f1f0285f62deda09e2f7c7b9a57c14
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-08 19:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelector('.image')
+  const parent = document.querySelector('#parent')
+
+  let draggedItem = null
+
+  items.forEach((item) => {
+    item.addEventListener('dragstart', () => {
+      draggedItem = item
+      item.classList.add('selected')
+    })
+
+    item.addEventListener('dragend', () => {
+      setTimeout(() => {
+        draggedItem.classList.remove('selected')
+        draggedItem = null
+      }, 0)
+    })
+
+    item.addEventListener('dragover', (e) => {
+      e.preventDefault()
+    })
+
+    item.addEventListener('drop', () => {
+      if (draggedItem) {
+        parent.insertBefore(draggedItem, item)
+      }
+    })
+  })
+})
